66b24af604176a9a00fb9853309b2436bc4f12c7
lnk <nakyeom0620@gmail.com>
2024-12-12 11:06:26 +0900

    웹소켓ã통신 원리

---
+++ src/ex01/MyClient.java
@@ -0,0 +1,30 @@
+package ex01;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MyClient {
+
+    public static void main(String[] args) throws IOException {
+        // 로컬 호스트와 포트 필요
+        Socket socket = new Socket("localhost", 10000);
+
+        // 밑에 주석달아놓은 코드보다 더 편한 방법
+        // autoFlush true 해놨으니까 flush 따로 명시 안 해도 됨
+        PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+        pw.println("Hello World");
+
+//        BufferedWriter bw = new BufferedWriter(
+//                new OutputStreamWriter(socket.getOutputStream())
+//        );
+//
+//        // 프로토콜(약속) - 문자열에 \n 꼭 붙이기
+//        // MyServer의 bufferdReader가 \n까지 읽기 때문에
+//        // bufferdWriter의 문자열에 \n을 꼭 포함해야 읽을 수 있음
+//        bw.write("Hello World\n");
+//        bw.flush();
+    }
+}
---
+++ src/ex01/MyServer.java
@@ -0,0 +1,35 @@
+package ex01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyServer {
+
+    public static void main(String[] args) {
+
+        try {
+            // 1. 서버 소켓 생성 (포트 번호 10000)
+            // 서버는 소켓을 두 개 만듦 => 소켓을 리스너(듣는거)라고 함
+            ServerSocket serverSocket = new ServerSocket(10000);
+
+            // 2. 리스닝 (while 돌면서 해당하는 포트 번호 찾아내는 중)
+            Socket socket = serverSocket.accept();
+            System.out.println("oh! connect?");
+
+            // 버퍼에 소켓 달기
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+
+            // 버퍼에 있는 메시지를 \n(개행)까지 읽음
+            String line = br.readLine();
+            System.out.println("read : " + line);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
